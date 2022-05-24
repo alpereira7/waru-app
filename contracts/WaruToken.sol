0945a2881d3d6f59bf76586b770075f4dca09007
alpereira7 <albertopereira7@gmail.com>
2022-05-24 15:36:23 +0200

    Update with SafeMath

--- contracts/WaruToken.sol
+++ contracts/WaruToken.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract WaruToken is Ownable, ERC20Snapshot {
+
+    using SafeMath for uint256;
 
     address public wtrAddress = 0x9Fdcf9E4C3b9c8606803FAdA2734FABda2D24Dc8;
     address public wtcAddress = 0x9Fdcf9E4C3b9c8606803FAdA2734FABda2D24Dc8;
@@ -28,24 +31,27 @@ contract WaruToken is Ownable, ERC20Snapshot {
     }
 
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        require(amount%1000 == 0, "You can only send a multiple of 1000 wei.");
+        require(amount.mod(1000) == 0, "You can only send a multiple of 1000 wei.");
         require(balanceOf(msg.sender) >= amount);
-        address owner = msg.sender;
-        _transfer(owner, wtrAddress, amount/1000);
-        _transfer(owner, wtcAddress, amount/1000);
-        _transfer(owner, to, amount-amount/500);
+        uint256 fee;
+        fee = amount.div(1000);
+        _transfer(msg.sender, wtrAddress, fee);
+        _transfer(msg.sender, wtcAddress, fee);
+        uint256 totalFee;
+        totalFee = fee.mul(2);
+        _transfer(msg.sender, to, amount.sub(totalFee));
         return true;
     }
 
     // Only Owner
     function transferNoFees(address to, uint256 amount) public onlyOwner callerIsUser {
         require(balanceOf(msg.sender) >= amount);
-        address owner = msg.sender;
-        _transfer(owner, to, amount);
+        _transfer(msg.sender, to, amount);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
-        require(totalSupply() + amount <= 21000000000000000000000000, "Can't mint more than 21 million tokens");
+        require(amount.add(totalSupply()) <= 21000000000000000000000000, "Can't mint more than 21 million tokens");
+        require(amount > 0);
         _mint(to, amount);
     }
 
