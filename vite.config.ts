baeedbe0cf780de29368495f579ea0875966e39f
Lucio <luciocc55@gmail.com>
2022-05-30 10:53:24 -0300

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import { resolve } from 'path'
 import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill';
 import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill';
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+  ],
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
@@ -16,12 +23,25 @@ export default defineConfig({
       ],
     },
   },
-  plugins: [
-    vue(),
-  ],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      preserveEntrySignatures: 'strict',
+      plugins: [
+        NodeModulesPolyfills(),
+        GlobalsPolyfills({
+          process: true,
+          buffer: true,
+        }),
+      ]
+    }
+  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
+      '@': resolve(__dirname, './src'),
+      '@walletconnect/web3-provider': resolve(__dirname, './node_modules/@walletconnect/web3-provider/dist/umd/index.min.js')
     },
   },
 })
