aea0920c9b38d271db98c00a30242331de89a518
alpereira7 <albertopereira7@gmail.com>
2022-06-07 21:26:50 +0200

    Lock compiler version

--- contracts/WaruToken.sol
+++ contracts/WaruToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.8.14;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
