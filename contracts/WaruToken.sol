1959909f1c59d2dcda2934573cd36cd4cccbdc7a
alpereira7 <albertopereira7@gmail.com>
2022-06-14 15:25:20 +0200

    Update after 0xGuard audit

--- contracts/WaruToken.sol
+++ contracts/WaruToken.sol
@@ -13,8 +13,8 @@ contract WaruToken is Ownable, ERC20Snapshot {
     address public wtrAddress = 0x672CeDBCE027E51888133312AaCaC2FF8e3614e6;
     address public wtcAddress = 0xC4ae728aC2a0f263A44A992Fd2B97798bE4342F0;
 
-    uint256 public MAX_SUPPLY = 21000000000000000000000000;
-    uint256 public INIT_SUPPLY = 7350000000000000000000000;
+    uint256 public constant MAX_SUPPLY = 21000000000000000000000000;
+    uint256 public constant INIT_SUPPLY = 7350000000000000000000000;
 
     constructor() ERC20("Waru Token", "WARU") {
         _mint(msg.sender, INIT_SUPPLY); // 7.35 million premint
@@ -36,25 +36,27 @@ contract WaruToken is Ownable, ERC20Snapshot {
 
     // Admin functions
  
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         require(amount.add(totalSupply()) <= MAX_SUPPLY, "Can't mint more than 21 million tokens.");
         require(amount > 0);
         _mint(to, amount);
     }
    
-    function setWtrAddress(address _wtrAddress) public onlyOwner {
+    function setWtrAddress(address _wtrAddress) external onlyOwner {
+        require(_wtrAddress != address(0), "Invalid address.");
         wtrAddress = _wtrAddress;
     }
     
-    function setWtcAddress(address _wtcAddress) public onlyOwner {
+    function setWtcAddress(address _wtcAddress) external onlyOwner {
+        require(_wtcAddress != address(0), "Invalid address.");
         wtcAddress = _wtcAddress;
     }
     
-    function snapshot() public onlyOwner {
+    function snapshot() external onlyOwner {
         _snapshot();
     }
     
-    function withdraw() public payable onlyOwner {
+    function withdraw() external payable onlyOwner {
         (bool success, ) = payable(msg.sender).call{
             value: address(this).balance
         }("");
